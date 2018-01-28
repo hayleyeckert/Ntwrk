7c042e610cfd5c1486dba24f427b1f75863693f2
hayleyeckert <hayleyeckert@tamu.edu>
2018-01-27 23:58:39 -0600

    Added Some Stuff

--- Ntwrk/MapController.swift
+++ Ntwrk/MapController.swift
@@ -5,18 +5,44 @@
 //  Created by Hayley Eckert on 1/27/18.
 //  Copyright © 2018 Hayley Eckert. All rights reserved.
 //
-
+class MSCCoordinates : NSObject, MKAnnotation{
+    var coordinate = CLLocationCoordinate2D(latitude: 30.6117, longitude: -96.3417)
+    var title: String? = "MSC"
+}
 import UIKit
 import MapKit
 import CoreLocation
+import UserNotifications
 
-class MapController: UIViewController {
-
+class MapController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager = CLLocationManager()
     @IBOutlet var mapView: MKMapView!
+    
+    let ENTERED_REGION_MESSAGE = "Entered MSC"
+    let EXITED_REGION_MESSAGE = "Leave MSC"
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let circleRenderer = MKCircleRenderer(overlay: overlay)
+        circleRenderer.fillColor = UIColor.blue
+        circleRenderer.strokeColor = UIColor.blue
+        circleRenderer.alpha = 0.5
+        circleRenderer.lineWidth = 1
+        return circleRenderer
+    }
+    
     override func viewDidLoad() {
-        let initiallocation = CLLocation(latitude: 30.6119, longitude: -96.3417)
-        centerMapONLocation(location: initiallocation)
         super.viewDidLoad()
+        self.mapView.delegate = self
+        locationManager.allowsBackgroundLocationUpdates = true
+        self.locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        mapView.showsUserLocation = true
+        mapView.showsCompass = true
+        mapView.showsScale = true
+        let initialLocation = CLLocation(latitude: 30.6119, longitude: -96.3417)
+        //mapView.addAnnotation(MSCCoordinates())
+        centerMapOnLocation(location: initialLocation)
+        setupData()
         // Do any additional setup after loading the view.
     }
 
@@ -26,12 +52,89 @@ class MapController: UIViewController {
     }
     
     let regionRadius : CLLocationDistance = 1000
-    func centerMapONLocation(location: CLLocation){
+    func centerMapOnLocation(location: CLLocation){
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
-        mapView.setRegion(coordinateRegion, animated: true)
+        self.mapView.setRegion(coordinateRegion, animated: true)
     }
     
-
+    @IBAction func zoomIn(_ sender: AnyObject) {
+        let userLocation = mapView.userLocation
+        
+        let region = MKCoordinateRegionMakeWithDistance(
+            userLocation.location!.coordinate, 2000, 2000)
+        
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func setupData() {
+        if CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) {
+            
+            // 2. region data
+            let title = "MSC"
+            let coordinate = CLLocationCoordinate2DMake(30.6122, -96.3417)
+            let regionRadius = 50.0
+            
+            // 3. setup region
+            let region = CLCircularRegion(center: CLLocationCoordinate2D(latitude: coordinate.latitude,
+                                                                         longitude: coordinate.longitude), radius: regionRadius, identifier: title)
+            region.notifyOnExit = true;
+            region.notifyOnEntry = true;
+            locationManager.startMonitoring(for: region)
+            
+            // 4. setup annotation
+            let restaurantAnnotation = MKPointAnnotation()
+            restaurantAnnotation.coordinate = coordinate;
+            restaurantAnnotation.title = "MSC";
+            mapView.addAnnotation(restaurantAnnotation)
+            
+            // 5. setup circle
+            let circle = MKCircle(center: coordinate, radius: regionRadius)
+            self.mapView.add(circle)
+            
+        }
+        else {
+            print("System can't track regions")
+        }
+    }
+ 
+    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
+        print("Started Monitoring Region: \(region.identifier)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print(ENTERED_REGION_MESSAGE)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print(EXITED_REGION_MESSAGE)
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                                  monitoringDidFailFor region: CLRegion?,
+                                  withError error: Error) {
+        print("MSC Fail")
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "MKAnnotation"
+        
+                let annotationView = MKPinAnnotationView(annotation:annotation, reuseIdentifier:identifier)
+                annotationView.isEnabled = true
+                annotationView.canShowCallout = true
+                
+                let btn = UIButton(type: .detailDisclosure)
+                annotationView.rightCalloutAccessoryView = btn
+                return annotationView
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        let ac = UIAlertController(title: "TAMUHack", message: "View Participants", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
